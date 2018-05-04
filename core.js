ffd377ea4574fbd0876afe4596ab392d8b0dc566
Lucas Miranda <liddack@outlook.com>
2018-05-04 15:29:08 -0300

    Minor changes and code comments

--- core.js
+++ core.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const log = require('fancy-log'),
-    jsdom = require('jsdom'),
-    { JSDOM } = jsdom
+      jsdom = require('jsdom'),
+      { JSDOM } = jsdom
 
+// Defines playback data fetched from MPC.
 let playback = {
     filename: '',
     position: '',
@@ -14,6 +15,8 @@ let playback = {
     prevPosition: '',
 }
 
+// Defines strings and image keys according to the 'state' string
+// provided by MPC.
 const states = {
     '-1': {
         string: 'Idling',
@@ -33,14 +36,22 @@ const states = {
     }
 }
 
+/**
+ * Sends Rich Presence updates to Discord client.
+ * @param {SnekfetchResponse} res Response from MPC Web Interface variables page
+ * @param {RPCClient} rpc Discord Client RPC connection instance
+ */
 const updatePresence = (res, rpc) => {
+    // Gets a DOM object based on MPC Web Interface variables page.
     let { document } = new JSDOM(res.body).window
 
+    // Gets relevant info from the DOM object.
     playback.filename = document.getElementById('file').textContent
     playback.state = document.getElementById('state').textContent
     playback.duration = sanitizeTime(document.getElementById('durationstring').textContent)
     playback.position = sanitizeTime(document.getElementById('positionstring').textContent)
 
+    // Prepares playback data for Discord Rich Presence.
     let payload = {
         state: playback.duration + ' total',
         startTimestamp: undefined,
@@ -50,6 +61,7 @@ const updatePresence = (res, rpc) => {
         smallImageText: states[playback.state].string
     }
 
+    // Makes changes to payload data according to playback state.
     switch (playback.state) {
         case '-1': // Idling
             payload.state = states[playback.state].string
@@ -63,21 +75,29 @@ const updatePresence = (res, rpc) => {
             break;
     }
 
+    // Only sends presence updates if playback state changes or if playback position
+    // changes while playing.
     if ((playback.state !== playback.prevState) || (
         playback.state === '2' &&
         convert(playback.position) !== convert(playback.prevPosition) + 5
     )) {
         rpc.setActivity(payload)
         log.info('INFO: Presence update sent: ' +
-            `CONNECTED - ${states[playback.state].string} - ${playback.position} / ${playback.duration} - ${playback.filename}`
+            `${states[playback.state].string} - ${playback.position} / ${playback.duration} - ${playback.filename}`
         )
     }
 
+    // Replaces previous playback state and position for later comparison.
     playback.prevState = playback.state
     playback.prevPosition = playback.position
     return true
 }
 
+/**
+ * Simple and quick utility to convert time from 'hh:mm:ss' format to seconds.
+ * @param {string} time Time string formatted as 'hh:mm:ss'
+ * @returns {number} Number of seconds converted from the given time string
+ */
 const convert = time => {
     let parts = time.split(':'),
         seconds = parseInt(parts[parts.length - 1]),
@@ -86,6 +106,12 @@ const convert = time => {
     return ((hours * 60 * 60) + (minutes * 60) + seconds)
 }
 
+/**
+ * In case the given 'hh:mm:ss' formatted time string time is less than 1 hour, 
+ * removes the '00' hours from it.
+ * @param {string} time Time string formatted as 'hh:mm:ss'
+ * @returns {string} Time string without '00' hours
+ */
 const sanitizeTime = time => {
     if (time.split(':')[0] === '00') {
         return time.substr(3, time.length - 1)
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ log.info('INFO: Loading...')
 
 const snekfetch = require('snekfetch'),
 	  { Client } = require('discord-rpc'),
-	  core = require('./core'),
+	  updatePresence = require('./core'),
 	  events = require('events'),
 	  config = require('./config'),
 	  clientID = '427863248734388224'
@@ -17,6 +17,7 @@ let mediaEmitter = new events.EventEmitter(),
 	mpcServerLoop,
 	rpc
 
+// Checks if port set in config.js is valid.
 if (isNaN(config.port)) {
 	throw new Error('Port is empty or invalid! Please set a valid port number in \'config.js\' file.')
 }
@@ -25,38 +26,53 @@ const uri = `http://localhost:${config.port}/variables.html`
 
 log.info('INFO: Fully ready. Trying to connect to Discord client...')
 
+// When it succesfully connects to MPC Web Interface, it begins checking MPC
+// every 5 seconds, getting its playback data and sending it to Discord Rich Presence
+// through updatePresence() function from core.js.
 mediaEmitter.on('CONNECTED', res => {
 	clearInterval(mpcServerLoop)
-	mpcServerLoop = setInterval(checkMedia, 5000)
+	mpcServerLoop = setInterval(checkMPCEndpoint, 5000)
 	if (!active) {
 		log.info('INFO: Connected to MPC-HC')
 	}
-	active = core(res, rpc)
+	active = updatePresence(res, rpc)
 })
 
+// When connection to MPC fails it attempts to connect
+// to MPC again every 15 seconds.
 mediaEmitter.on('CONN_ERROR', code => {
 	log.error(`ERROR: Unable to connect to Media Player Classic on port ${config.port}. ` +
 		`Make sure MPC is running, Web Interface is enabled and the port set in 'config.js' file is correct.\n` + code)
+	// If MPC was previously connected (ie. MPC gets closed while script is running)
+	// the whole process is killed and restarted by Forever in order to clean Rich Presence
+	// from user's profile, as destroyRPC() apparently can't do so.
 	if (active) {
 		process.exit(0)
 	}
-	clearInterval(mpcServerLoop)
-	mpcServerLoop = setInterval(checkMedia, 15000)
+	if (mpcServerLoop._onTimeout !== checkMPCEndpoint) {
+		clearInterval(mpcServerLoop)
+		mpcServerLoop = setInterval(checkMPCEndpoint, 15000)
+	}
 })
 
+// If RPC successfully connects to Discord client,
+// this script attempts to connect to MPC Web Interface every 15 seconds. 
 mediaEmitter.on('discordConnected', () => {
 	clearInterval(discordRPCLoop)
 	log.info('INFO: Connected to Discord. Listening MPC on ' + uri)
-	checkMedia()
-	mpcServerLoop = setInterval(checkMedia, 15000)
+	checkMPCEndpoint()
+	mpcServerLoop = setInterval(checkMPCEndpoint, 15000)
 })
 
+// When RPC gets disconnected from Discord Client,
+// this script stops checking MPC playback data.
 mediaEmitter.on('discordDisconnected', () => {
 	clearInterval(mpcServerLoop)
 })
 
-// Functions
-function checkMedia() {
+// Tries to connect to Media Player Classic Web Interface and,
+// if connected, fetches its data.
+function checkMPCEndpoint() {
 	snekfetch.get(uri)
 		.then(function (res) {
 			mediaEmitter.emit('CONNECTED', res)
@@ -66,20 +82,18 @@ function checkMedia() {
 		})
 }
 
+// Initiates a new RPC connection to Discord client.
 function initRPC(clientID) {
 	rpc = new Client({ transport: 'ipc' });
-
 	rpc.on('ready', () => {
 		clearInterval(discordRPCLoop)
 		mediaEmitter.emit('discordConnected')
-
 		rpc.transport.once('close', async () => {
 			await destroyRPC();
 			log.error('ERROR: Connection to Discord has closed. Trying again in 10 seconds...');
 			mediaEmitter.emit('discordDisconnected')
 			discordRPCLoop = setInterval(initRPC, 10000, clientID);
 		});
-		//rpc.setActivity(payload);
 	})
 
 	// Log in to the RPC Client, and check whether or not it errors.
@@ -88,11 +102,13 @@ function initRPC(clientID) {
 	})
 }
 
+// Destroys any active RPC connection.
 async function destroyRPC() {
 	if (!rpc) return;
 	await rpc.destroy();
 	rpc = null;
-	log.info('INFO: você destruiu o meu rpc');
 }
 
+// This line boots the whole script, attempting to connect
+// to Discord client every 10 seconds.
 discordRPCLoop = setInterval(initRPC, 10000, clientID);
