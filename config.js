c661489a865ee610808c42fea79e4a2a5de1e86b
Lucas Miranda <liddack@outlook.com>
2019-11-15 15:07:28 -0300

    Add short description for each `config.js` option

--- config.js
+++ config.js
@@ -1,6 +1,28 @@
+// See README.md to learn more on each option
+
+// Port on which MPC Web Interface is running.
+// Default: 13579
 exports.port = 13579
+
+// Whether to omit brackets [] and its content in filenames.
+// Default: true
 exports.ignoreBrackets = true
+
+// Whether to omit filetype.
+// Default: false
 exports.ignoreFiletype = false
+
+// Whether to replace _ with space.
+// Default: true
 exports.replaceUnderscore = true
+
+// Whether to replace dot (.) characters
+// (except the file extension one) with spaces.
+// Default: true
 exports.replaceDots = true
+
+// Whether to display the current file's remaining
+// playback time while playing, instead of showing
+// the elapsed time.
+// Default: false
 exports.showRemainingTime = false
